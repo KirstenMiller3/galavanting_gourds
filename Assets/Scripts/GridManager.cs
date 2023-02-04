b10146e13f6e33d82c6239c049bb22c72af0dd10
mattg <msgrainger1@gmail.com>
2023-02-04 09:43:22 +0000

    Movement update

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,41 +5,64 @@ using UnityEngine.Experimental.GlobalIllumination;
 
 public class GridManager : MonoBehaviour
 {
+    public struct GridSquare
+    {
+        public Transform Transform;
+        public bool IsOccupied;
+    }
+
     [SerializeField] private Transform[] _points;
     [SerializeField] private int _rowSize = 5;
 
-    public Transform[] Points => _points;
-
-    private Transform[,] _grid;
+    private GridSquare[,] _grid;
 
     private Vector2Int _gridPos = new Vector2Int();
 
     private void Awake()  {
-        _grid = new Transform[_rowSize, _rowSize];
+        _grid = new GridSquare[_rowSize, _rowSize];
         for(int i = 0; i  < _points.Length; i++) {
             int row = (int)(i / _rowSize);
             int col = (int)(i % _rowSize);
-            _grid[row, col] = _points[i];
-        }    
+            _grid[row, col].Transform = _points[i];
+        }
+
+        _grid[0, 0].IsOccupied = true;
     }
 
-    public void Move(Vector2Int movement)
+    public bool Move(Vector2Int movement, bool isOccupied)
     {
-        Debug.Log($"Move: {movement}");
+        Vector2Int desiredPos = new Vector2Int();
+        desiredPos = _gridPos;
+        desiredPos += movement;
 
-        _gridPos += movement;
+        desiredPos.x = Mathf.Clamp(desiredPos.x, 0, _rowSize - 1);
+        desiredPos.y = Mathf.Clamp(desiredPos.y, 0, _rowSize - 1);
 
-        Debug.Log($"1: {_gridPos}");
+        if (_grid[desiredPos.x, desiredPos.y].IsOccupied)
+        {
+            return false;
+        }
+
+        _gridPos = desiredPos;
+        Debug.Log(_gridPos);
+        _grid[_gridPos.x, _gridPos.y].IsOccupied = isOccupied;
+        return true;
+    }
+
+    public void UndoMove(Vector2Int movement)
+    {
+        _grid[_gridPos.x, _gridPos.y].IsOccupied = false;
+
+        _gridPos -= movement;
 
         _gridPos.x = Mathf.Clamp(_gridPos.x, 0, _rowSize - 1);
         _gridPos.y = Mathf.Clamp(_gridPos.y, 0, _rowSize - 1);
-
-        Debug.Log(_gridPos);
     }
+
 
     public Vector3 GetPosition()
     {
-        Vector3 gridPos = _grid[_gridPos.x, _gridPos.y].position;
+        Vector3 gridPos = _grid[_gridPos.x, _gridPos.y].Transform.position;
         return new Vector3(gridPos.x, 0f, gridPos.z);
     }
 }
--- Assets/Scripts/RootController.cs
+++ Assets/Scripts/RootController.cs
@@ -1,31 +1,80 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RootController : MonoBehaviour {
-    [SerializeField] private GridManager _gridManager;  
-    [SerializeField] private float _inputPollSpeed = 0.1f;
+public class RootController : MonoBehaviour
+{
+    public struct BodySection
+    {
+        public GameObject body;
+        public Vector2Int movement;
+    }
 
-    private float _inputTimer = 0f;
+    [SerializeField] private GridManager _gridManager;
+    [SerializeField] private GameObject _bodySection;
+
+    private Stack<BodySection> _body = new Stack<BodySection>();
 
     public void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W)) {
-            _gridManager.Move(Vector2Int.up);
-        }
-        else if(Input.GetKeyDown(KeyCode.S))
-        {
-            _gridManager.Move(Vector2Int.down);
-        }
-        else if(Input.GetKeyDown(KeyCode.D)) { 
-            _gridManager.Move(Vector2Int.right); 
-        }  
-        else if(Input.GetKeyDown(KeyCode.A)) { 
-            _gridManager.Move(Vector2Int.left);    
-        }
-
-        //Debug.Log(_gridManager.GetPosition());
-        transform.position = _gridManager.GetPosition();
+        InputUpdate();
     }
 
+    private void InputUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Move(Vector2Int.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            Move(Vector2Int.left);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Undo();
+        }
+    }
+
+    private void Move(Vector2Int movement)
+    {
+        if(_gridManager.Move(movement, true))
+        {
+            SpawnBodySection(movement);
+            Vector3 newPos = _gridManager.GetPosition();
+            transform.position = newPos;
+        }
+    }
+
+    private void SpawnBodySection(Vector2Int movement)
+    {
+        GameObject _section = Instantiate(_bodySection, transform.position, Quaternion.identity);
+        _body.Push(new BodySection() { body = _section, movement = movement });
+    }
+
+    private void Undo()
+    {
+        if (_body.Count == 0)
+        {
+            return;
+        }
+
+        GameObject partToUndo = _body.Peek().body;
+        Destroy(partToUndo);
+
+        _gridManager.UndoMove(_body.Peek().movement);
+        transform.position = _gridManager.GetPosition();
+
+        _body.Pop();
+    }
 }
