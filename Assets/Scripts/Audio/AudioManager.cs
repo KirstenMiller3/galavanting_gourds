ca43197250d9e9c03c7064e87321f2dc9f8f77b0
BOSSA001\User <kirstenmiller16@gmail.com>
2023-02-05 13:19:24 +0000

    audio toggle sliders added to audiomanager prefab

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -10,6 +10,10 @@ public class AudioManager : MonoBehaviour
 
     public Sound[] sounds;
 
+    public GameObject PausePanel;
+
+    private bool pausePanelToggle = true;
+
     public static AudioManager instance;
     void Awake()
     {
@@ -40,6 +44,17 @@ public class AudioManager : MonoBehaviour
 
 
         Play("main");
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PausePanel.SetActive(pausePanelToggle);
+            pausePanelToggle = !pausePanelToggle;
+        }
 
     }
 
